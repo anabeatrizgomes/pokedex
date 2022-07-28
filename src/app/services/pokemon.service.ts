23d600f621f5f7d6b5b7ca322d2315941ff016f7
anabeatrizgomes <anabiagomes2104@gmail.com>
2022-07-28 14:53:58 -0300

    2 files modified

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,10 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  public listaPokemon = [];
   private url = 'https://pokeapi.co/api/v2/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  buscarTodosPokemon(){
+    return this.http.get(this.url);
+    
+  }
 }
