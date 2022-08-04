69e461197905733421c1c0e803cb7e24cc208331
anabeatrizgomes <anabiagomes2104@gmail.com>
2022-08-04 15:02:11 -0300

    2 files modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -15,15 +15,27 @@ export class Tab1Page implements OnInit {
   constructor(public pokemonService: PokemonService) {}
 
   ngOnInit(){
-    this.pokemonService.buscarTodosPokemon().subscribe((dados) =>{ 
-      this.total = dados ['count'];
-      this.next = dados ['next'];
-      this.previous = dados ['previous'];
-      this.listaPokemon = dados['results'];
-      console.log(dados);
-    });
+   this.buscarPokemon();
 
     }
   
+ async buscarPokemon(){
+  this.pokemonService.buscarTodosPokemon().subscribe((dados) =>{ 
+    this.total = dados ['count'];
+    this.next = dados ['next'];
+    this.previous = dados ['previous'];
+    //this.listaPokemon = dados['results'];
+    console.log(dados);
+    for(let pokemon of dados['results']){
+      this.pokemonService
+      .buscarUmPokemon(pokemon.url)
+      .subscribe((dadosPokemon) => {
+        this.listaPokemon.push(dadosPokemon);
+      });
+        
+      
 
+    }
+  });
+ }
 }
